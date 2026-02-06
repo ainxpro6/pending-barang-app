0f222072172096f7a5db91e21323f1d8cd69999c
ainxpro6 <ainxpro6@gmail.com>
2026-02-06 15:26:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQ5RNkhmmjBoEdFXi3CVRGzP8DXWEGx3Q",
+  authDomain: "updatepending-df0da.firebaseapp.com",
+  projectId: "updatepending-df0da",
+  storageBucket: "updatepending-df0da.firebasestorage.app",
+  messagingSenderId: "639417251832",
+  appId: "1:639417251832:web:2a85b55bc042dd17f4480d",
+  measurementId: "G-472359ZVFX"
+};
